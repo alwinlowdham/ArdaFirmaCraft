3987c330fe03c47940eab1da2b07a8a50be2751b
alwinlowdham <87349843+alwinlowdham@users.noreply.github.com>
2021-08-03 19:34:01 -0500

    Delete src directory

--- src/main/java/com/alwinlowdham/afc/Ref.java
+++
@@ -1,5 +0,0 @@
-package com.alwinlowdham.afc;
-
-public class Ref {
-		public static final String MOD_ID = "afc";
-}
--- src/main/java/com/alwinlowdham/afc/interfaces/IBlood.java
+++
@@ -1,8 +0,0 @@
-package com.alwinlowdham.afc.interfaces;
-
-import net.minecraft.util.DamageSource;
-
-public interface IBlood {
-    int getTicksPerBeat();
-    boolean bleed(float amount);
-}
